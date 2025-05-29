efcbd7dbdcc49d9df9502b2f1817194bbc52a265
azkuang <alexzkuang0314@gmail.com>
2025-05-28 23:36:25 -0400

    updated nvim config

--- lua/alexk/packer.lua
+++ lua/alexk/packer.lua
@@ -19,6 +19,15 @@ return require('packer').startup(function(use)
   use ('mbbill/undotree')
   use ('tpope/vim-fugitive')
 
+  -- lua/plugins/rose-pine.lua
+  use {
+	  "rose-pine/neovim",
+	  as = "rose-pine",
+	  config = function()
+		  vim.cmd("colorscheme rose-pine")
+	  end
+  }
+
   use {
 	  'VonHeikemen/lsp-zero.nvim',
 	  requires = {
--- lua/alexk/remap.lua
+++ lua/alexk/remap.lua
@@ -1,3 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>s", vim.cmd.vsplit)
+vim.keymap.set("n", "<leader>nm", function() vim.cmd("set number") end)
