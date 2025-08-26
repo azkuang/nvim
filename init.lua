9cc19cef4f9b7e340492e8e27f27f00d1117be9d
azkuang <alexzkuang0314@gmail.com>
2025-08-26 11:31:25 -0400

    Updates to nvim config

--- init.lua
+++ init.lua
@@ -2,4 +2,4 @@ require("alexk")
 require 'nvim-treesitter.install'.compilers = { "gcc", "zig" }
 print("Write some code!!!!!")
 vim.wo.number = true
-vim.wo.relative = true
+vim.wo.relativenumber = true
